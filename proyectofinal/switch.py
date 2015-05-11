83c98d278778c0be7c9fd6f559a3a72d3597444f
Alu0100702545 <alu0100702545@ull.edu.es>
2015-05-11 21:22:01 +0000

    pene

--- proyectofinal/switch.py
+++ proyectofinal/switch.py
@@ -39,9 +39,9 @@ class L2Forwarding(app_manager.RyuApp):
 	tabla_vlan = { 1: '10',
 				   2: '10',
 				   3: '20',
-				   4: '20'
-	}
+				   4: '20'}
 	
+
 	def __init__(self, *args, **kwargs):
 		super(L2Forwarding, self).__init__(*args, **kwargs)
 
@@ -65,56 +65,24 @@ class L2Forwarding(app_manager.RyuApp):
 
 		dst = eth.dst  
         
-        #Extramos la MAC de origen
+		#Extramos la MAC de origen
         
 		src = eth.src
         
 		if src not in self.mac_to_port.keys():
 			self.mac_to_port[src]=in_port
-			
-		if dst not in self.mac_to_port.keys():
-			actions = [ofp_parser.OFPPacketOut(ofproto.OFPP_FLOOD)]
-			req = ofp_parser.OFPPacketOut(datapath=datapath, buffer_id=None, in_port=in_port, actions=actions, data=msg.data)
-			datapath.send_msg(req)
-			
-		elif haddr_to_bin(dst) == mac.BROADCAST or mac.is_multicast(haddr_to_bin(dst)):
+	
+		if haddr_to_bin(dst) == mac.BROADCAST or mac.is_multicast(haddr_to_bin(dst)):
+			print("BROADCAST")
 			# Creamos el conjunto de acciones: FLOOD
-			actions = [ofp_parser.OFPActionOutput(ofproto.OFPP_FLOOD)]
-	
-			# Ahora creamos el match  
-			# fijando los valores de los campos 
-			# que queremos casar.
-			match = ofp_parser.OFPMatch(eth_dst=dst)
-	
-			# Creamos el conjunto de instrucciones.
-			inst = [ofp_parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)]
-	
-			# Creamos el mensaje OpenFlow 
-			mod = ofp_parser.OFPFlowMod(datapath=datapath, priority=0, match=match, instructions=inst, idle_timeout=30, buffer_id=msg.buffer_id)
-	
-			# Enviamos el mensaje.
-			datapath.send_msg(mod)
-			
-		elif self.tabla_vlan[self.mac_to_port[src][0]]==self.tabla_vlan[self.mac_to_port[dst][0]]:
-			
-			actions = [ofp_parser.OFPActionOutput(self.mac_to_port[dst])]
-			# Ahora creamos el match  
-			# fijando los valores de los campos 
-			# que queremos casar.
-			match = ofp_parser.OFPMatch(eth_dst=dst)
-			
-			# Creamos el conjunto de instrucciones.
-			inst = [ofp_parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)]
-			
-			# Creamos el mensaje OpenFlow 
-			mod = ofp_parser.OFPFlowMod(datapath=datapath, priority=0, match=match, instructions=inst, idle_timeout=30, buffer_id=msg.buffer_id)
-			
-			# Enviamos el mensaje.
-			datapath.send_msg(mod)
-
-		else: #En caso contrario, tiramos el paquete
 			actions = []
 	
+			for j in self.tabla_vlan.keys():
+				print("Puerto? ", j)
+				if self.tabla_vlan.get(j)==self.tabla_vlan.get(in_port) and j !=in_port :
+					actions.append(ofp_parser.OFPActionOutput(j))
+					print("Vlan ", self.tabla_vlan.get(j),"puerto de entrada", in_port, "otros puertos ", j)
+			
 			# Ahora creamos el match  
 			# fijando los valores de los campos 
 			# que queremos casar.
@@ -128,4 +96,54 @@ class L2Forwarding(app_manager.RyuApp):
 	
 			# Enviamos el mensaje.
 			datapath.send_msg(mod)
-
+	
+		elif dst not in self.mac_to_port.keys():
+			#actions = [ofp_parser.OFPPacketOut(ofproto.OFPP_FLOOD)]
+			actions = []
+			print("NO CONOZCO LA MAC ", dst)
+			
+			for i in self.tabla_vlan.keys():
+				if self.tabla_vlan.get(i)==self.tabla_vlan.get(in_port) and i !=in_port :
+					actions.append(ofp_parser.OFPActionOutput(i))
+					print i
+			
+			print("QUIEN DE LA VLAN ", self.tabla_vlan.get(in_port), " TIENE LA MAC ", dst,"?")
+			#Aqui esta el fallo en buffer id os dejo el mensaje que sale
+			
+			req = ofp_parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id, in_port=in_port, actions=actions, data=msg.data)
+			datapath.send_msg(req)
+			
+		elif self.tabla_vlan.get(self.mac_to_port.get(src)) == self.tabla_vlan.get(self.mac_to_port.get(dst)) :
+			actions = [ofp_parser.OFPActionOutput(self.mac_to_port[dst])]
+			print("Pasando paquetes de la vlan ", self.tabla_vlan.get(self.mac_to_port.get(src)))
+			# Ahora creamos el match  
+			# fijando los valores de los campos 
+			# que queremos casar.
+			match = ofp_parser.OFPMatch(eth_dst=dst)
+	
+			# Creamos el conjunto de instrucciones.
+			inst = [ofp_parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)]
+	
+			# Creamos el mensaje OpenFlow 
+			mod = ofp_parser.OFPFlowMod(datapath=datapath, priority=0, match=match, instructions=inst, idle_timeout=30, buffer_id=msg.buffer_id)
+	
+			# Enviamos el mensaje.
+			datapath.send_msg(mod)
+			
+		else: #Si no son de la misma vlan, tira el paquete
+			actions = []
+			print("Tirando paquete!!")
+	
+			# Ahora creamos el match  
+			# fijando los valores de los campos 
+			# que queremos casar.
+			match = ofp_parser.OFPMatch(eth_dst=dst)
+	
+			# Creamos el conjunto de instrucciones.
+			inst = [ofp_parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)]
+	
+			# Creamos el mensaje OpenFlow 
+			mod = ofp_parser.OFPFlowMod(datapath=datapath, priority=0, match=match, instructions=inst, idle_timeout=30, buffer_id=msg.buffer_id)
+	
+			# Enviamos el mensaje.
+			datapath.send_msg(mod)
